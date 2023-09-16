5f973b7511cd3cb10a07fa76b9cdd9e31e890ea3
Sandes P <73277007+sandes438@users.noreply.github.com>
2023-09-16 21:29:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     if (username == 'sarang' && password == "myPassword#68") {
-        window.location.href = "file:///home/user/Desktop/website/pilldispenser/home.html";
+        window.location.href = "https://sandes438.github.io/pillboxweb/home.html";
     } else {
         alert("You have entered wrong credentials")
     }
